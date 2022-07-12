6eabdaf087da24b3e40316562a7f77abc34852ce
Giangkhamliu <grace21@navgurukul.org>
2022-07-12 11:47:00 +0530

    y first commit

--- basic_sum.py
+++ basic_sum.py
@@ -1,4 +1,7 @@
-num=int(input("Enter the number"))
-num1=int(input("Enter the number"))
-sum=num+num1
-print(sum)
+# num=int(input("Enter the number"))
+# num1=int(input("Enter the number"))
+# sum=num+num1
+# print(sum)
+a=6
+b=7
+print(a,b)
